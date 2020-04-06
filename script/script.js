bcf5cd0654ce8212fd08ea5de4ba2c4bedbe716d
Pranesh Palanisamy <ppranesh5@gmail.com>
2020-04-07 00:43:45 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,13 +1,16 @@
-var randomNumber1 = (Math.floor((Math.random()*6)+1));
+// to get the values 1~6
+var randomNumber1 = (Math.floor((Math.random()*6)+1));  
 var randomNumber2 = (Math.floor((Math.random()*6)+1));
 
+// concatinating with the resources
 document.querySelector(".dice .img1").setAttribute("src","/Dicee/images/dice"+randomNumber1+".png");
 document.querySelector(".dice .img2").src = "/Dicee/images/dice"+randomNumber2+".png";
 
+// comparing the results
 if (randomNumber1 < randomNumber2) {
     document.getElementsByTagName("h1")[0].innerHTML = "Player 2 Wins! 🚴🏻‍♂️";
 } else if (randomNumber1 > randomNumber2) {
     document.getElementsByTagName("h1")[0].innerHTML = "🚴🏻‍♂️ Player 1 Wins!";
 } else {
-    document.getElementsByTagName("h1")[0].innerHTML = "🚴🏻‍♂️ Draw! ";
+    document.getElementsByTagName("h1")[0].innerHTML = "🚴🏻‍♂️ Draw! 🚴🏻‍♂️ ";
 }
