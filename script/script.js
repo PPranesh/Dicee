7e772a64a2097d1717cba06336e4b037ab6b502a
Pranesh Palanisamy <ppranesh5@gmail.com>
2020-04-07 00:22:36 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -5,9 +5,9 @@ document.querySelector(".dice .img1").setAttribute("src","/Dicee/images/dice"+ra
 document.querySelector(".dice .img2").src = "/Dicee/images/dice"+randomNumber2+".png";
 
 if (randomNumber1 < randomNumber2) {
-    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 Wins! 🚴‍♀";
+    document.getElementsByTagName("h1")[0].innerHTML = "Player 2 Wins! 🚴🏻‍♂️";
 } else if (randomNumber1 > randomNumber2) {
-    document.getElementsByTagName("h1")[0].innerHTML = "🚴‍♀️ Player 1 Wins!";
+    document.getElementsByTagName("h1")[0].innerHTML = "🚴🏻‍♂️ Player 1 Wins!";
 } else {
-    document.getElementsByTagName("h1")[0].innerHTML = "🚴‍♀ Draw! 🚴‍♀";
+    document.getElementsByTagName("h1")[0].innerHTML = "🚴🏻‍♂️ Draw! ";
 }
